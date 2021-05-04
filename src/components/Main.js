8958a1887093b3a9b0c529bc27eba9bee5301d9f
Nikita Dannik <nikitka.dannik@gmail.com>
2021-05-04 10:55:24 +0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import Card from './Card';
-import {api} from '../utils/Api'; 
+import {api} from '../utils/api'; 
 
 function Main(props) {
   const [userName, setUserName] = React.useState("");
